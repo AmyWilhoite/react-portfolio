d9ae1b5df9a4c76e28ed3e9cc3cc3f4e574ac231
AmyWilhoite <amywilhoite@gmail.com>
2022-05-30 15:58:40 -0400

    Updates

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-// import ContactFormComponent from './components/contact_form.component';
-
-
-const App = () => <PortfolioContainer />;
-
-export default App;
